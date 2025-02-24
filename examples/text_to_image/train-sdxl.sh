f944e7b499f3ae48ab43706dbc07b31c8ad1a09a
Gee <gee.castro@gendo.ai>
2025-02-24 17:03:36 +0000

    add train-sdxl.sh

---
+++ examples/text_to_image/train-sdxl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
+export VAE_NAME="madebyollin/sdxl-vae-fp16-fix"
+export DATASET_NAME="lambdalabs/naruto-blip-captions"
+
+echo "Starting training script for SDXL..."
+echo "Using model: $MODEL_NAME"
+echo "Using VAE: $VAE_NAME"
+echo "Using dataset: $DATASET_NAME"
+
+accelerate launch train_text_to_image_sdxl.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_vae_model_name_or_path=$VAE_NAME \
+  --dataset_name=$DATASET_NAME \
+  --enable_xformers_memory_efficient_attention \
+  --resolution=512 --center_crop --random_flip \
+  --proportion_empty_prompts=0.2 \
+  --train_batch_size=32 \
+  --gradient_accumulation_steps=4 --gradient_checkpointing \
+  --max_train_steps=10000 \
+  --use_8bit_adam \
+  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --mixed_precision="fp16" \
+  --validation_prompt="a cute Sundar Pichai creature" --validation_epochs 5 \
+  --checkpointing_steps=5000 \
+  --output_dir="sdxl-naruto-model"
